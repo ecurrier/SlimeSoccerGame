23f79c58521277fb9d2ec82bef5ed8ea84d1addf
Evan Currier <Evan Currier>
2016-07-28 14:33:11 -0500

    initial commit

---
+++ android/src/com/slimesoccer/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.slimesoccer.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.slimesoccer.game.MainGameClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGameClass(), config);
+	}
+}
---
+++ desktop/src/com/slimesoccer/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.slimesoccer.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.slimesoccer.game.MainGameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGameClass(), config);
+	}
+}
