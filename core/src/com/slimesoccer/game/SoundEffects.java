42c4548790835dd7b477727f431058e44d4a2c15
Evan Currier <ecurri3@gmail.com>
2016-11-18 15:15:12 -0600

    Fixed out of bound errors and files not being found

--- core/src/com/slimesoccer/game/SoundEffects.java
+++ core/src/com/slimesoccer/game/SoundEffects.java
@@ -7,6 +7,11 @@ import com.badlogic.gdx.audio.Sound;
 
 public class SoundEffects {
 
+	Sound bounce1 = Gdx.audio.newSound(Gdx.files.internal("Sounds/ball-bounce1.wav"));
+	Sound bounce2 = Gdx.audio.newSound(Gdx.files.internal("Sounds/ball-bounce2.wav"));
+	Sound slime1 = Gdx.audio.newSound(Gdx.files.internal("Sounds/slime-contact1.wav"));
+	Sound slime2 = Gdx.audio.newSound(Gdx.files.internal("Sounds/slime-contact2.wav"));
+	Sound crowd1 = Gdx.audio.newSound(Gdx.files.internal("Sounds/crowd1.mp3"));
 	
 	Sound[] bounceSounds = {bounce1, bounce2};
 	Sound[] slimeSounds = {slime1, slime2};
@@ -20,14 +25,17 @@ public class SoundEffects {
 	}
 	
 	public void BallBounce() {
+		int index = ThreadLocalRandom.current().nextInt(0, bounceSounds.length);
 		bounceSounds[index].play(1.0f);
 	}
 	
 	public void SlimeContact() {
+		int index = ThreadLocalRandom.current().nextInt(0, slimeSounds.length);
 		slimeSounds[index].play(1.0f);
 	}
 	
 	public void CrowdStart() {
+		int index = ThreadLocalRandom.current().nextInt(0, crowdSounds.length);
 		currentCrowdSound = crowdSounds[index];
 		
 		currentCrowdId = currentCrowdSound.play(1.0f);
